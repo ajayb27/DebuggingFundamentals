f3b2f3a96ef61e7af28cb66b3e27727e2e30f8ed
Ajay <iamajaybathula@gmail.com>
2023-05-02 22:57:32 +0530

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() !=  typeof(Product)) return false;
+            Product p = (Product) obj;
+            return this.Name.Equals(p.Name) && this.Price == p.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
